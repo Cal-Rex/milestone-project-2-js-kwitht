51017ec0febf03814b4f4029443129fe0b4a0806
Cal-Rex <andy.gibb@healthcareskills.com>
2022-10-04 13:51:24 +0000

    add js for initial page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("Game start menu")
+function pageLoad() {
+ let stats = document.querySelectorAll('.start')
+ stats.forEach(stat => {
+    stat.style.visibility ="hidden";
+ });
+    
+}
+
+window.addEventListener('load', pageLoad);
+
+document.getElementById('intro-text').textContent = 
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+     ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`;
