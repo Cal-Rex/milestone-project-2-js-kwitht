98002b4219d309ee3b0237b8522f2b34aaedcb17
Cal-Rex <andy.gibb@healthcareskills.com>
2022-10-05 10:41:09 +0000

    add function that generates random quest story

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,23 @@
 const knightButton = document.getElementById('choose-knight');
 const thiefButton = document.getElementById('choose-thief');
 const merchButton = document.getElementById('choose-merchant');
+let job = "";
 
 /**removes game controls on page inital load so that users can only select options that start the game */
 function pageLoad() {
- let stats = document.querySelectorAll('.load')
- stats.forEach(stat => {
-    stat.style.visibility ="hidden";
- });
- let restartButton = document.getElementById('new-game');
- restartButton.style.visibility = "hidden";
+   let stats = document.querySelectorAll('.load')
+   stats.forEach(stat => {
+      stat.style.visibility = "hidden";
+   });
+   let restartButton = document.getElementById('new-game');
+   restartButton.style.visibility = "hidden";
 }
 
 window.addEventListener('load', pageLoad);
 
 // explanatory text on how the game works
-document.getElementById('intro-text').textContent = 
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+document.getElementById('intro-text').textContent =
+   `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
@@ -27,43 +28,77 @@ document.getElementById('intro-text').textContent =
 // 3 functions below allow player to pick their job before starting their quest using the 3 job buttons
 /** selects the Knight Job, allocates necessary stats and begins the quest */
 function rollKnight() {
-   document.getElementById('str-num').textContent = "4"; 
-   document.getElementById('dex-num').textContent = "3"; 
-   document.getElementById('cun-num').textContent = "2"; 
+   document.getElementById('str-num').textContent = "4";
+   document.getElementById('dex-num').textContent = "3";
+   document.getElementById('cun-num').textContent = "2";
+   job = "Knight"
    startGame()
+   createQuest()
 }
 
 /** selects the Thief Job, allocates necessary stats and begins the quest */
 function rollThief() {
-   document.getElementById('str-num').textContent = "2"; 
-   document.getElementById('dex-num').textContent = "6"; 
-   document.getElementById('cun-num').textContent = "3"; 
+   document.getElementById('str-num').textContent = "2";
+   document.getElementById('dex-num').textContent = "6";
+   document.getElementById('cun-num').textContent = "3";
+   job = "Thief"
    startGame()
+   createQuest()
 }
 
 /** selects the Merchant Job, allocates necessary stats and begins the quest */
 function rollMerch() {
-   document.getElementById('str-num').textContent = "3"; 
-   document.getElementById('dex-num').textContent = "2"; 
-   document.getElementById('cun-num').textContent = "4"; 
+   document.getElementById('str-num').textContent = "3";
+   document.getElementById('dex-num').textContent = "2";
+   document.getElementById('cun-num').textContent = "4";
+   job = "Merchant"
    startGame()
+   createQuest()
 }
 
 /** removes job selector buttons and replaces with game controls */
 function startGame() {
-    let stats = document.querySelectorAll('.load')
- stats.forEach(stat => {
-    stat.style.visibility ="visible";
- });
- let choices = document.querySelectorAll('.start')
- choices.forEach(choice => {
-    choice.style.visibility ="hidden";
- });
-    
+   let stats = document.querySelectorAll('.load')
+   stats.forEach(stat => {
+      stat.style.visibility = "visible";
+   });
+   let choices = document.querySelectorAll('.start')
+   choices.forEach(choice => {
+      choice.style.visibility = "hidden";
+   });
+}
+
+function createQuest() {
+   let questLine = {
+      name: ["Harrison", "Brick", "Engelbert", "Constance", "Inga", "Gertrude"],
+      endDestination: ["the Grand ol' Opry", "the Bahamas", "your Gran's house",
+         "Greggs", "the Amish Lowrider Motor Show", "your scheduled audience with the king"
+      ],
+      reason: ["as is tradition", "for tax purposes", "on a spiritual journey of self-discovery",
+         "for an important business lunch", "for a romantic long weekend", "because you have been paid a lot of money to do so"
+      ]
+   }
+   let x = []
+   let y = []
+   let z = []
+   for (let i = 0; i < 3; i++){
+     let d6 = (Math.round((Math.floor(Math.random() * 11)) / 100 * 50));
+     if (x.length === 0){
+       x.push(d6);
+       } else if (y.length === 0){
+       y.push(d6);
+       } else if (z.length === 0){
+       z.push(d6);
+       } else {
+         console.log("no variables to assign dice roll, breaking loop!")
+         break;
+     }
+   }
+   document.getElementById('intro-text').textContent = 
+    `You are ${questLine.name[x]} the ${job} and you are travelling to ${questLine.endDestination[y]}, ${questLine.reason[z]}!`;
 }
 
 // event listeners for the 3 job buttons that start the quests
 knightButton.addEventListener('click', rollKnight);
 thiefButton.addEventListener('click', rollThief);
 merchButton.addEventListener('click', rollMerch);
-
