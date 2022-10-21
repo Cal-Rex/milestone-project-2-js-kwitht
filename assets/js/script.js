dd9b2a01dbd29c1c3842b13cc4d1a9ec4935eaa8
Cal-Rex <andy.gibb@healthcareskills.com>
2022-10-21 19:24:19 +0000

    bugfix: z-index issue with elements #modal-box and #modal-button layering

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,13 +136,6 @@ span.onclick = function() {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}; 
-
 // Own code added to accomodate touchscreens for modal box
 function touchScreenModalHandler() {
    modal.style.display = "block";
